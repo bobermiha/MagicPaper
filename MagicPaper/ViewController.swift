6337b703186098261922f7fd28a5aa9e01573451
Mikhail Bobrov <bobermiha@yandex.ru>
2022-10-29 18:45:50 +0500

    fix

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -9,66 +9,89 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController {
+    
+    @IBOutlet var sceneView: ARSCNView! {
+        didSet {
+            sceneView.delegate = self
+            //            sceneView.showsStatistics = true
+        }
+    }
+    
+    private var videoNode = SKVideoNode()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackedImages = ARReferenceImage.referenceImages(
+            inGroupNamed: "NewspaperImages",
+            bundle: Bundle.main)
+        {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 1
+            print("Images successfully found")
+        } else {
+            print("Thre is no images or some error occured")
+        }
+        
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+}
+
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        guard let imageAnchor = anchor as? ARImageAnchor,
+              let videoName = imageAnchor.referenceImage.name else { return nil}
+        
+        
+        videoNode = SKVideoNode(fileNamed: "\(videoName).mp4")
+        
+        videoNode.play()
+        
+        let videoScene = SKScene(size: CGSize(width: 1280, height: 720))
+        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+        videoNode.yScale = -1.0
+        videoScene.scaleMode = .aspectFit
+        videoScene.addChild(videoNode)
+    
+        
+        
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = videoScene
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        node.addChildNode(planeNode)
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        if node.isHidden == true {
+            if let imageAnchor = anchor as? ARImageAnchor {
+                sceneView.session.remove(anchor: imageAnchor)
+                videoNode.pause()
+            }
+        } else {
+            videoNode.play()
+        }
     }
 }
